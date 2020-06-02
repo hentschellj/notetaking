b992f46e5170b822e1c0a26624ac6aaf88f3133c
hentschellj <hentschellj@gmail.com>
2020-06-02 15:14:18 -0500

    Ensured working connection to mongo, fleshed out post request

--- notes/routes.js
+++ notes/routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const NoteModel = require('./model')
 
 router.get('/', (req, res, next)=>{
   res.send('Get all notes')
@@ -9,7 +10,23 @@ router.get('/:id', (req, res, next)=>{
 })
 
 router.post('/', (req, res, next)=>{
-  res.send('Create note')
+  const newNote = new NoteModel({
+    title: 'New Note',
+    body: 'This is a new note'
+  })
+  newNote
+    .save()
+    .then((document)=>{
+      if(document) {
+        res.json(document)
+      } else {
+        res.send('Document did not save')
+      }
+    })
+    .catch((err)=>{
+      console.log(err)
+      res.send('Error Occurred')
+    })
 })
 
 router.put('/:id', (req, res, next)=>{
